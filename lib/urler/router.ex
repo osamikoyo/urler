022523f373d2d1830efdedde9d003a31c014758c
osamikoyo <v.pylayev@mail.ru>
2025-03-19 07:05:10 +0500

    update routers

--- lib/urler/router.ex
+++ lib/urler/router.ex
@@ -1,6 +1,13 @@
 defmodule Urler.Router do
   use Plug.Router
 
+  get "/urls/watch" do
+    urs = Urler.Repo.all()
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, JasonV.encode!(urs))
+  end
+
   match _ do
     send_resp(conn, 404, "not found")
   end
