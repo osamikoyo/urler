64fb9682065f580dd8e926065106155521e214a3
osamikoyo <v.pylayev@mail.ru>
2025-03-19 07:44:31 +0500

    add insert handler

--- lib/urler/router.ex
+++ lib/urler/router.ex
@@ -8,6 +8,27 @@ defmodule Urler.Router do
     |> send_resp(200, JasonV.encode!(urs))
   end
 
+  post "urls/add" do
+    {:ok, body, conn} = conn |> read_body()
+
+    attrs = JasonV.decode!(body)
+
+    changeset = Urler.Url.changeset(%Urler.Url{}, attrs)
+
+    case Urler.Repo.insert(changeset) do
+      {:ok, url} ->
+        conn
+        |> put_resp_content_type("text/plain")
+        |> send_resp(200, "ok")
+      {:error, _} ->
+        conn
+        |> put_resp_content_type("text/plain")
+        |> send_resp(400, "cant insert url")
+    end
+
+
+  end
+
   match _ do
     send_resp(conn, 404, "not found")
   end
